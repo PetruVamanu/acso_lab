8c5fa62950a20aaaf6ecfa2893839e173ad13acd
PetruVamanu <PetruVamanu@users.noreply.github.com>
2025-12-09 20:48:46 +0000

    deploy: 422a85ad648f2e85331031341659193086516980

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-d18e6e95.js',
+                path_to_root + 'searchindex-3f6c8c00.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
