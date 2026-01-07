ccc3d7f273d3c7c2907ae1a26ed36d30c1dc9dd0
PetruVamanu <PetruVamanu@users.noreply.github.com>
2026-01-07 19:51:39 +0000

    deploy: 57cd3dfa4d5cd7ea3f92bd186d7428ccf0f16cd9

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-bdeb983b.js',
+                path_to_root + 'searchindex-26ea49c6.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
